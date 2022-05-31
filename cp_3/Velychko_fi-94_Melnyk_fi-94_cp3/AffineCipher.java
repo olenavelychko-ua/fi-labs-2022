ab171c034c4aed4ec7cadb63035db11c2d581b1b
Olena Velychko <evkissa@gmail.com>
2022-05-31 15:08:43 +0300

    working version

--- cp_3/Velychko_fi-94_Melnyk_fi-94_cp3/AffineCipher.java
+++ cp_3/Velychko_fi-94_Melnyk_fi-94_cp3/AffineCipher.java
@@ -22,14 +22,47 @@ public class AffineCipher {
         return temp;
     }
 
-    public static int EuclidsAlgorithm(int a, int b) {
+    public static int getKeyByChar1(char orig) {
+        int temp = (int) orig - 1072;
+        if (temp > 25) temp--;
+        return temp;
+    }
+
+    public static int getKeyByChar2(char orig) {
+        int temp = (int) orig - 1072;
+        if (temp > 25) temp--;
+        if (temp == 26) temp++;
+        else if (temp == 27) temp--;
+        return temp;
+    }
+
+    public static char getCharByKey1(int key) {
+        if (key > 24) key += 1;
+        return (char) (key + 1072);
+    }
+
+    public static char getCharByKey2(int key) {
+        if (key == 26) key += 1;
+        else if (key == 27) key -= 1;
+        if (key > 25) key += 1;
+        return (char) (key + 1072);
+    }
+
+    public static int modularArithmetic(int num, int mod) {
+        while (num > mod) num = num - mod;
+        while (num < 0) num = num + mod;
+        return num;
+    }
+
+    public static int[] EuclidsAlgorithm(int a, int b) {
+        int[] gcdAndrev = new int[2];
         int u2, v2, r, q;
         int rev_a = 0;
         int u0 = 1;
         int v0 = 0;
         int u1 = 0;
         int v1 = 1;
-        int n = b;
+        int n=b;
         r = 1;
         while (b != 1) {
             q = a / b;
@@ -48,47 +81,41 @@ public class AffineCipher {
             v1 = v2;
             rev_a = u2;
         }
-        if (rev_a < 0) {
-            rev_a = n + rev_a;
-        }
-        return rev_a;
+        rev_a = modularArithmetic(rev_a, n);
+        gcdAndrev[0] = r;
+        gcdAndrev[1] = rev_a;
+        return gcdAndrev;
     }
 
     public static ArrayList<Integer> congruenceSolution(int a, int b, int n) {
         ArrayList<Integer> answers = new ArrayList<Integer>();
-        int bConst = b;
-        int k = a;
-        int koef = 1;
-        int rev_a;
+        int[] gcdAndrev = new int[2];
+        gcdAndrev = EuclidsAlgorithm(a, n);
+        int k = gcdAndrev[0];
+        int i = 1;
+        int rev_a = gcdAndrev[1];
         int x = 0;
-        while (k > 0) {
-            if (a % k == 0 && n % k == 0) {
-                a = a / k;
-                n = n / k;
-                koef = koef * k;
-            }
-            k--;
-        }
-        if (koef == 1) {
-            rev_a = EuclidsAlgorithm(a, n);
+        if (k == 1) {
             x = rev_a * b;
-            while (x > n) x = x - n;
+            x = modularArithmetic(x, n);
             answers.add(x);
         }
-        if (koef != 1) {
-            if (bConst % koef != 0) {
+        if (k != 1) {
+            a = a / k;
+            n = n / k;
+            if (b % k != 0) {
                 answers.add(0);
                 System.out.println(("Congruence has no solutions!"));
                 return answers;
-            } else if (bConst % koef == 0) {
-                b = b / koef;
-                rev_a = EuclidsAlgorithm(a, n);
-                x = rev_a * b;
-                while (x > n) x = x - n;
-                while (koef > 0) {
-                    int temp = x + (koef - 1) * n;
+            } else if (b % k == 0) {
+                b = b / k;
+                gcdAndrev = EuclidsAlgorithm(a, n);
+                x = gcdAndrev[1] * b;
+                x = modularArithmetic(x, n);
+                while (i < k) {
+                    int temp = x + (i - 1) * n;
                     answers.add(temp);
-                    koef--;
+                    i++;
                 }
             }
         }
@@ -100,6 +127,7 @@ public class AffineCipher {
         for (int k = 0; k + 1 < text.length(); k++) {
             String bigram = text.substring(k, k + 2);
             mapbi.put(bigram, mapbi.getOrDefault(bigram, 0) + 1);
+            //k++;
         }
         HashMap<String, Integer> hm1 = sortByValue(mapbi);
         mapbi.clear();
@@ -107,8 +135,9 @@ public class AffineCipher {
         for (String str : hm1.keySet()) {
             mapbi.put(str, hm1.get(str));
             q += 1;
-            if (q == 5) break;
+            if (q == 25) break;
         }
+        System.out.println(mapbi);
         return (mapbi);
     }
 
@@ -116,61 +145,108 @@ public class AffineCipher {
         StringBuilder arrayBi = new StringBuilder();
         ArrayList<Integer> answers = new ArrayList<Integer>();
         ArrayList<Integer> aAndb = new ArrayList<Integer>();
-        char[] arrayLang = {'с', 'н', 'о', 'т', 'о', 'н', 'а', 'е', 'н'};
+        ArrayList<Integer> aAll = new ArrayList<Integer>();
+        int[] gcdAndrev = new int[2];
+        char[] arrayLang = {'с', 'т', 'н', 'о', 'т', 'о', 'н', 'а', 'е', 'н'};
         int m = (int) Math.pow(31, 2);
-        int x;
-        int y;
-        int a1, a, b, yBi1, xBi1, yBi2, xBi2;
+        int x, y, b, yBi1, xBi1, yBi2, xBi2;
         for (String str : mapbi1.keySet()) {
             arrayBi.append(str);
-            System.out.println(str);
+            //System.out.println(str);
         }
-        System.out.println(arrayBi);
-        xBi1=(arrayBi.charAt(0)-1072)*31+(arrayBi.charAt(1)-1072);
-        xBi2=(arrayBi.charAt(1)-1072)*31+(arrayBi.charAt(2)-1072);
-        yBi1=(arrayLang[0]-1072)*31+(arrayLang[1]-1072);
-        yBi2=(arrayLang[2]-1072)*31+(arrayLang[3]-1072);
-        x = xBi1-xBi2;
-        y = yBi1-yBi2;
-        answers = congruenceSolution(y, x, m);
-        a1 = answers.get(0);
-        a = EuclidsAlgorithm(a1, m);
-        int temp = yBi1 - a * xBi1;
-        answers = congruenceSolution(1, temp, m);
-        b = answers.get(0);
+        for (int i = 0; i < 8; i += 2) {
+            for (int j = 0; j < 2; j += 2) {
 
-        aAndb.add(a);
-        aAndb.add(b);
+                    for (int k = j + 2; k < 50; k += 2) {
+
+                        xBi1 = getKeyByChar2(arrayLang[i]) * 31 + getKeyByChar2(arrayLang[i + 1]);
+                        System.out.println(arrayLang[i] + " " + arrayLang[i + 1]);
+                        xBi2 = getKeyByChar2(arrayLang[i+2]) * 31 + getKeyByChar2(arrayLang[i+3]);
+                        System.out.println(arrayLang[i+2] + " " + arrayLang[i + 3]);
+                        yBi1 = getKeyByChar2(arrayBi.charAt(j)) * 31 + getKeyByChar2(arrayBi.charAt(j + 1));
+                        System.out.println(arrayBi.charAt(j) + " " + arrayBi.charAt(j + 1));
+                        yBi2 = getKeyByChar2(arrayBi.charAt(k)) * 31 + getKeyByChar2(arrayBi.charAt(k + 1));
+                        System.out.println(arrayBi.charAt(k) + " " + arrayBi.charAt(k + 1));
+                        x = xBi1 - xBi2;
+                        System.out.println(xBi1 + " xB " + xBi2);
+                        x = modularArithmetic(x, m);
+                        System.out.println("x " + x);
+                        y = yBi1 - yBi2;
+                        System.out.println(yBi1 +" yB1 " + yBi2);
+                        y = modularArithmetic(y, m);
+                        System.out.println("y " + y);
+                        aAll = congruenceSolution(x, y, m);
+                        System.out.println("a all " + aAll);
+                        if(aAll.get(0) != 0){
+                        gcdAndrev=EuclidsAlgorithm(aAll.get(0),m);
+                        if (aAll.size() == 1 && gcdAndrev[0]==1 ) {
+                            b = yBi1 - aAll.get(0) * xBi1;
+                            b = modularArithmetic(b, m);
+                            System.out.println("b " + b);
+                            aAndb.add(aAll.get(0));
+                            aAndb.add(b);
+                        }
+                        } else if (aAll.size() > 1) {
+                            int c = 0;
+                            while (c < aAll.size()) {
+                                gcdAndrev=EuclidsAlgorithm(aAll.get(c),m);
+                                if(gcdAndrev[0]==1 && aAll.get(c) != 0) {
+                                    b = yBi1 - aAll.get(c) * xBi1;
+                                    b = modularArithmetic(b, m);
+                                    System.out.println("b " + b);
+                                    aAndb.add(aAll.get(c));
+                                    aAndb.add(b);
+
+                                }
+                                c++;
+                            }
+                        }
+                        System.out.println("A and b " + aAndb);
+
+                    }
+
+            }
+        }
         return aAndb;
-
     }
+
 
     public static String decryption(String text) {
         LinkedHashMap<String, Integer> biMap = new LinkedHashMap<>();
+        ArrayList<Integer> keyParamsCheck = new ArrayList<Integer>();
         ArrayList<Integer> keyParams = new ArrayList<Integer>();
-        StringBuilder pureText = new StringBuilder();
         StringBuilder text1 = new StringBuilder();
+        StringBuilder pureText = new StringBuilder();
+        int[] gcdAndrev = new int[2];
         text1.append(text);
+        System.out.println(text1);
         int rev_a;
         int y, x1, x2;
-        int i = 0;
         int m = (int) Math.pow(31, 2);
         biMap = find5MostPopularBigrams(text);
         keyParams = findingKey(biMap);
-        rev_a = EuclidsAlgorithm(keyParams.get(0), m);
-        System.out.println(text1);
-        while (pureText.length() != text.length()) {
-            y=text1.charAt(i)*31+text1.charAt(i+1);
-            int temp;
-            System.out.println(text1.charAt(i)+" "+ text1.charAt(i+1));
-            temp = rev_a * (y - keyParams.get(1));
-            while (temp> m) temp = temp - m;
-            x1=temp/31;
-            x2=temp%31;
-            pureText.append((char) (x1+1072));
-            pureText.append((char) (x2+1072));
-            i++;
+        System.out.println(keyParams + "++");
+        for (int q = 0; q < keyParams.size(); q += 2) {
+            int i = 0;
+            StringBuilder pipiText = new StringBuilder();
+            gcdAndrev  = EuclidsAlgorithm(keyParams.get(q), m);
+            while (pipiText.length() != text.length()) {
+                y = getKeyByChar2(text1.charAt(i)) * 31 + getKeyByChar2(text1.charAt(i + 1));
+                //System.out.println(y);
+                int temp;
+                //System.out.println(text1.charAt(i) + " " + text1.charAt(i + 1));
+                temp = gcdAndrev [1] * (y - keyParams.get(q + 1));
+                temp = modularArithmetic(temp, m);
+                x1 = temp / 31;
+                x2 = temp % 31;
+                pipiText.append(getCharByKey2(x1));
+                pipiText.append(getCharByKey2(x2));
+                i += 2;
+            }
+            System.out.println(pipiText.toString());
+
         }
+
         return pureText.toString();
     }
 
@@ -183,5 +259,10 @@ public class AffineCipher {
         }
         String pureText = decryption(text.toString());
         System.out.println(pureText);
+        /*
+        int m=961;
+        ArrayList answ= new ArrayList();
+        answ=congruenceSolution(a, b, m);
+        System.out.println(answ); */
     }
 }
